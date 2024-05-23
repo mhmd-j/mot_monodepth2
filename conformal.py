624baf8fc3f6c8d3b981ce273902de112ca4919a
mhmd-j <mo.jani1378@gmail.com>
2024-05-23 14:25:38 -0700

    CP works online and loading from file

--- conformal.py
+++ conformal.py
@@ -37,7 +37,7 @@ def calculate_pixelwise_nonconformity_scores(y_pred, gt_depth, min_depth=1e-3, m
         crop = np.array([0.40810811 * gt_height, 0.99189189 * gt_height,
                             0.03594771 * gt_width,  0.96405229 * gt_width]).astype(np.int32)
         crop_mask = np.zeros(mask.shape)
-        crop_mask[crop[0]:crop[1], crop[2]:crop[3]] = 1 #FIXME: not sure if this is correct
+        crop_mask[:, crop[0]:crop[1], crop[2]:crop[3]] = 1 #FIXME: not sure if this is correct
         mask = np.logical_and(mask, crop_mask)
         
     nonconformity_scores = np.abs(gt_depth - y_pred) * mask  # Absolute error at each pixel
@@ -160,65 +160,67 @@ def evaluate(opt):
     assert sum((opt.eval_mono, opt.eval_stereo)) == 1, \
         "Please choose mono or stereo evaluation by setting either --eval_mono or --eval_stereo"
 
+    # if opt.ext_disp_to_eval is None:
+
+    opt.load_weights_folder = os.path.expanduser(opt.load_weights_folder)
+
+    assert os.path.isdir(opt.load_weights_folder), \
+        "Cannot find a folder at {}".format(opt.load_weights_folder)
+
+    print("-> Loading weights from {}".format(opt.load_weights_folder))
+
+    val_filenames = readlines(os.path.join(os.path.dirname(__file__), "splits", opt.split, "val_files.txt"))
+    
+    encoder_path = os.path.join(opt.load_weights_folder, "encoder.pth")
+    decoder_path = os.path.join(opt.load_weights_folder, "depth.pth")
+    pose_encoder_path = os.path.join(opt.load_weights_folder, "pose_encoder.pth")
+    pose_decoder_path = os.path.join(opt.load_weights_folder, "pose.pth")
+    
+    encoder_dict = torch.load(encoder_path)
+    
+    img_ext = '.png' if opt.png else '.jpg'
+    dataset = datasets.KITTIRAWDataset(opt.data_path, val_filenames,
+                                        encoder_dict['height'], encoder_dict['width'],
+                                        frame_idxs = [0, 1], num_scales=4, is_train=False, img_ext=img_ext)
+    dataloader = DataLoader(dataset, opt.batch_size, shuffle=False, num_workers=opt.num_workers,
+                            pin_memory=True, drop_last=False)
+
+    encoder = networks.ResnetEncoder(opt.num_layers, False)
+    depth_decoder = networks.DepthDecoder(encoder.num_ch_enc)
+    pose_encoder = networks.ResnetEncoder(opt.num_layers, False, 2)
+    pose_decoder = networks.PoseDecoder(pose_encoder.num_ch_enc, 1, 2)
+    
+    model_dict = encoder.state_dict()
+    encoder.load_state_dict({k: v for k, v in encoder_dict.items() if k in model_dict})
+    depth_decoder.load_state_dict(torch.load(decoder_path))
+    pose_encoder.load_state_dict(torch.load(pose_encoder_path))
+    pose_decoder.load_state_dict(torch.load(pose_decoder_path))
+
+    encoder.cuda()
+    encoder.eval()
+    depth_decoder.cuda()
+    depth_decoder.eval()
+    pose_encoder.cuda()
+    pose_encoder.eval()
+    pose_decoder.cuda()
+    pose_decoder.eval()
+
+    pred_disps = []
+    pred_poses = []
+    gt_depths = []
+    
+    nonconformity_scores = []
+    print("-> Computing predictions with size {}x{}".format(
+        encoder_dict['width'], encoder_dict['height']))
+    
+    if opt.save_pred_disps:
+        model_name = opt.load_weights_folder.split("/")[-1]
+        depth_save_dir = os.path.join("results", opt.split, model_name, 'depth')
+        os.makedirs(depth_save_dir, exist_ok=True)
+        
+    opt.frame_ids = [0, 1]  # pose network only takes two frames as input
+    
     if opt.ext_disp_to_eval is None:
-
-        opt.load_weights_folder = os.path.expanduser(opt.load_weights_folder)
-
-        assert os.path.isdir(opt.load_weights_folder), \
-            "Cannot find a folder at {}".format(opt.load_weights_folder)
-
-        print("-> Loading weights from {}".format(opt.load_weights_folder))
-
-        val_filenames = readlines(os.path.join(os.path.dirname(__file__), "splits", opt.split, "val_files.txt"))
-        
-        encoder_path = os.path.join(opt.load_weights_folder, "encoder.pth")
-        decoder_path = os.path.join(opt.load_weights_folder, "depth.pth")
-        pose_encoder_path = os.path.join(opt.load_weights_folder, "pose_encoder.pth")
-        pose_decoder_path = os.path.join(opt.load_weights_folder, "pose.pth")
-        
-        encoder_dict = torch.load(encoder_path)
-        
-        img_ext = '.png' if opt.png else '.jpg'
-        dataset = datasets.KITTIRAWDataset(opt.data_path, val_filenames,
-                                           encoder_dict['height'], encoder_dict['width'],
-                                           frame_idxs = [0, 1], num_scales=4, is_train=False, img_ext=img_ext)
-        dataloader = DataLoader(dataset, opt.batch_size, shuffle=False, num_workers=opt.num_workers,
-                                pin_memory=True, drop_last=False)
-
-        encoder = networks.ResnetEncoder(opt.num_layers, False)
-        depth_decoder = networks.DepthDecoder(encoder.num_ch_enc)
-        pose_encoder = networks.ResnetEncoder(opt.num_layers, False, 2)
-        pose_decoder = networks.PoseDecoder(pose_encoder.num_ch_enc, 1, 2)
-        
-        model_dict = encoder.state_dict()
-        encoder.load_state_dict({k: v for k, v in encoder_dict.items() if k in model_dict})
-        depth_decoder.load_state_dict(torch.load(decoder_path))
-        pose_encoder.load_state_dict(torch.load(pose_encoder_path))
-        pose_decoder.load_state_dict(torch.load(pose_decoder_path))
-
-        encoder.cuda()
-        encoder.eval()
-        depth_decoder.cuda()
-        depth_decoder.eval()
-        pose_encoder.cuda()
-        pose_encoder.eval()
-        pose_decoder.cuda()
-        pose_decoder.eval()
-
-        pred_disps = []
-        pred_poses = []
-        gt_depths = []
-        nonconformity_scores = []
-        print("-> Computing predictions with size {}x{}".format(
-            encoder_dict['width'], encoder_dict['height']))
-        
-        if opt.save_pred_disps:
-            model_name = opt.load_weights_folder.split("/")[-1]
-            depth_save_dir = os.path.join("results", opt.split, model_name, 'depth')
-            os.makedirs(depth_save_dir, exist_ok=True)
-            
-        opt.frame_ids = [0, 1]  # pose network only takes two frames as input
-
         with torch.no_grad():
             for indx, data in enumerate(dataloader):
                 input_color = data[("color", 0, 0)].cuda()
@@ -272,76 +274,107 @@ def evaluate(opt):
                 #     transformation_from_parameters(axisangle[:, 0], translation[:, 0]).cpu().numpy())
                 
                 print_progress(indx + 1, len(dataloader), prefix='Progress:', suffix='Complete', length=50)
-            
-
-
-
-        # pred_disps = np.concatenate(pred_disps)
-        # pred_poses = np.concatenate(pred_poses)
-        # gt_depths = np.concatenate(gt_depths)
+                # pred_disps = np.concatenate(pred_disps)
+                # pred_poses = np.concatenate(pred_poses)
+                # gt_depths = np.concatenate(gt_depths)
+                
         nonconformity_scores = np.concatenate(nonconformity_scores)
+        np.save('nonconformity_scores.npy', nonconformity_scores)
         lower_bound, upper_bound = get_pixelwise_prediction_interval_std(pred_depth[0], nonconformity_scores)
         if opt.vis_depth:
-            plot_depth_with_uncertainty(pred_depth, lower_bound, upper_bound, gt_depth)
+            plot_depth_with_uncertainty(pred_depth[0], lower_bound, upper_bound, gt_depth)
+
 
     else:
-        # Load predictions from file
-        print("-> Loading predictions from {}".format(opt.ext_disp_to_eval))
-        pred_disps = np.load(opt.ext_disp_to_eval)
-
-        if opt.eval_eigen_to_benchmark:
-            eigen_to_benchmark_ids = np.load(
-                os.path.join(splits_dir, "benchmark", "eigen_to_benchmark_ids.npy"))
-
-            pred_disps = pred_disps[eigen_to_benchmark_ids]
+        # Load nonconformity score from file
+        print("-> Loading nonconformity score from {}".format(opt.ext_disp_to_eval))
+        nonconformity_scores = np.load(opt.ext_disp_to_eval)
+        
+        # Choose a random input from the dataset
+        with torch.no_grad():
+            np.random.seed(0)
+            inout_ind = np.random.randint(0, len(dataset))
+            sample_input = dataset[500]
+            sample_input_color = sample_input[("color", 0, 0)].unsqueeze(0).cuda()
             
-    if opt.save_pred_poses:
-        line_pred_poses = pred_poses.reshape(-1, 16)
-        model_name = opt.load_weights_folder.split("/")[-1]
-        save_path = os.path.join("results", opt.split, model_name, "pred_motion")
-        os.makedirs(save_path, exist_ok=True)
-        np.savetxt(os.path.join(save_path, "pred_motion.txt"), line_pred_poses)
+            for i in opt.frame_ids:
+                sample_input[("color_aug", i, 0)] = sample_input[("color_aug", i, 0)].unsqueeze(0).cuda()
+            if opt.post_process:
+                # Post-processed results require each image to have two forward passes
+                sample_input_color = torch.cat((sample_input_color, torch.flip(sample_input_color, [3])), 0)
+            sample_output = depth_decoder(encoder(sample_input_color))
+            sample_pred_disp, _ = disp_to_depth(sample_output[("disp", 0)], opt.min_depth, opt.max_depth)
+            
+            sample_gt_depth = sample_input["depth_gt"].squeeze().numpy()
+            sample_gt_height, sample_gt_width = sample_gt_depth.shape
+            # Interpolate the predicted disparity to the ground truth size
+            sample_pred_disp = torch.nn.functional.interpolate(
+                sample_pred_disp, [sample_gt_height, sample_gt_width], mode="bilinear", align_corners=False)
+            
+            sample_pred_disp = sample_pred_disp.cpu().squeeze().numpy()
+            sample_pred_depth = 1 / sample_pred_disp
+            
+            all_color_aug = torch.cat([sample_input[("color_aug", i, 0)] for i in opt.frame_ids], 1)
+            sample_features = [pose_encoder(all_color_aug)]
+            sample_axisangle, sample_translation = pose_decoder(sample_features)
+            
+            sample_pred_depth *= 35 # this the median of the mean of the gt_depths over one sequence
+            
+            lower_bound, upper_bound = get_pixelwise_prediction_interval_std(sample_pred_depth, nonconformity_scores)
 
-    # if opt.save_pred_disps:
-    #     output_path = os.path.join(
-    #         opt.load_weights_folder, "disps_{}_split.npy".format(opt.eval_split))
-    #     print("-> Saving predicted disparities to ", output_path)
-    #     np.save(output_path, pred_disps)
 
-    if opt.no_eval:
-        print("-> Evaluation disabled. Done.")
-        quit()
+        if opt.vis_depth:
+            plot_depth_with_uncertainty(sample_pred_depth, lower_bound, upper_bound, sample_gt_depth)
 
-    elif opt.eval_split == 'benchmark':
-        save_dir = os.path.join(opt.load_weights_folder, "benchmark_predictions")
-        print("-> Saving out benchmark predictions to {}".format(save_dir))
-        if not os.path.exists(save_dir):
-            os.makedirs(save_dir)
 
-        for idx in range(len(pred_disps)):
-            disp_resized = cv2.resize(pred_disps[idx], (1216, 352))
-            depth = STEREO_SCALE_FACTOR / disp_resized
-            depth = np.clip(depth, 0, 80)
-            depth = np.uint16(depth * 256)
-            save_path = os.path.join(save_dir, "{:010d}.png".format(idx))
-            cv2.imwrite(save_path, depth)
+            
+    # if opt.save_pred_poses:
+    #     line_pred_poses = pred_poses.reshape(-1, 16)
+    #     model_name = opt.load_weights_folder.split("/")[-1]
+    #     save_path = os.path.join("results", opt.split, model_name, "pred_motion")
+    #     os.makedirs(save_path, exist_ok=True)
+    #     np.savetxt(os.path.join(save_path, "pred_motion.txt"), line_pred_poses)
 
-        print("-> No ground truth is available for the KITTI benchmark, so not evaluating. Done.")
-        quit()
+    # # if opt.save_pred_disps:
+    # #     output_path = os.path.join(
+    # #         opt.load_weights_folder, "disps_{}_split.npy".format(opt.eval_split))
+    # #     print("-> Saving predicted disparities to ", output_path)
+    # #     np.save(output_path, pred_disps)
 
-    print("-> Evaluating")
+    # if opt.no_eval:
+    #     print("-> Evaluation disabled. Done.")
+    #     quit()
 
-    if opt.eval_stereo:
-        print("   Stereo evaluation - "
-              "disabling median scaling, scaling by {}".format(STEREO_SCALE_FACTOR))
-        opt.disable_median_scaling = True
-        opt.pred_depth_scale_factor = STEREO_SCALE_FACTOR
-    else:
-        print("   Mono evaluation - using median scaling")
+    # elif opt.eval_split == 'benchmark':
+    #     save_dir = os.path.join(opt.load_weights_folder, "benchmark_predictions")
+    #     print("-> Saving out benchmark predictions to {}".format(save_dir))
+    #     if not os.path.exists(save_dir):
+    #         os.makedirs(save_dir)
 
-    errors = []
-    ratios = []
-    nonconformity_scores = []
+    #     for idx in range(len(pred_disps)):
+    #         disp_resized = cv2.resize(pred_disps[idx], (1216, 352))
+    #         depth = STEREO_SCALE_FACTOR / disp_resized
+    #         depth = np.clip(depth, 0, 80)
+    #         depth = np.uint16(depth * 256)
+    #         save_path = os.path.join(save_dir, "{:010d}.png".format(idx))
+    #         cv2.imwrite(save_path, depth)
+
+    #     print("-> No ground truth is available for the KITTI benchmark, so not evaluating. Done.")
+    #     quit()
+
+    # print("-> Evaluating")
+
+    # if opt.eval_stereo:
+    #     print("   Stereo evaluation - "
+    #           "disabling median scaling, scaling by {}".format(STEREO_SCALE_FACTOR))
+    #     opt.disable_median_scaling = True
+    #     opt.pred_depth_scale_factor = STEREO_SCALE_FACTOR
+    # else:
+    #     print("   Mono evaluation - using median scaling")
+
+    # errors = []
+    # ratios = []
+    # nonconformity_scores = []
     # gt_local_poses = []
 
     # for i in range(1, pred_disps.shape[0]):
